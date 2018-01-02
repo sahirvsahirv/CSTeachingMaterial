b7555dc248a2bec36aaa701353b3fc30a8be260f
sahirvsahirv <gowri.r@gmail.com>
2018-01-02 15:35:32 +0530

    get post complete

--- Flask/flaskappnotonvenv/samplewebapp.py
+++ Flask/flaskappnotonvenv/samplewebapp.py
@@ -19,12 +19,18 @@ def showmapofcountry(countryname):
     print("should show showmap.html")
     return render_template('showmap.html', name = countryname)
 
+#POST is required to post from here to showmap.html
+@app.route("/showmap",methods= ['GET', 'POST'])
+def showmapcountry():
+    print("should show showmap.html")
+    return render_template('showmap.html')
+
 @app.route("/changedatainjs/<jsdata>")
 def changedataandsenditbacktopython(jsdata):
     print("in changedataandsenditbacktopython")
     return jsdata
 #not working
-@app.route("/changeinjs2', methods= ['POST']")
+@app.route("/changeinjs2", methods= ['POST'])
 def changedatainjs():
            jsdata = request.form
            #can also call render_template to call another html and pass a parameter
@@ -38,7 +44,7 @@ def getaddressofuser():
         jslocdata = request.form['data']
         print(jslocdata)
         #render - to redirect
-        return redirect(url_for('showmapofcountry', countryname = jslocdata))
+        return redirect(url_for('showmapcountry'))
         #this page getlocation is posting data to another page showmap.html
         #return render_template('showmap.html', countryname=jslocdata)
     else:
