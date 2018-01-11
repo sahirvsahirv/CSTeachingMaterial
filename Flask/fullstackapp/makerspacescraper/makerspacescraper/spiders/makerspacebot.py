860b1fa482f4083ed3f5fe38f3bb9fe2cd560f1c
sahirvsahirv <gowri.r@gmail.com>
2018-01-11 13:12:36 +0530

    scrapyworkspace

--- Flask/fullstackapp/makerspacescraper/makerspacescraper/spiders/makerspacebot.py
+++ Flask/fullstackapp/makerspacescraper/makerspacescraper/spiders/makerspacebot.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.shell import insepect_response
+from scrapy.shell import inspect_response
+import logging
 
 class MakerspacebotSpider(scrapy.Spider):
     name = 'makerspacebot'
-    allowed_domains = ['https://diyhacking.com/makerspaces/']
-    start_urls = ['http://https://diyhacking.com/makerspaces//']
+    allowed_domains = ['www.diyhacking.com/makerspaces/']
+    start_urls = ['https://www.diyhacking.com/makerspaces//']
 
     def parse(self, response):
         #view the HTML to see how the data looks
-        tablecontents = response.css(".mk-fancy-table.mk-shortcode table-style2").extract()
-        for row in response.xpath('//table/tbody/tr/'):
+        tablecontents = response.css().extract()
+        #/html/body/table/tbody/tr[332]/td[2]/span[2]/a
+        for row in response.xpath('/html/body/table/tbody/tr'):
             makerspaces = {}
-            makerspaces['country'] = row.xpath('td[1]//text()').extract()
-        yield tablecontents
+            from scrapy.shell import inspect_response
+            inspect_response(response, self)
+            logging.log(DEBUG, row)
+            makerspaces['makerspacelink'] = row.xpath('td[2]/span[2]/a').extract()
+            yield makerspaces
 
 
 
